068d209c46a9605aef3b72f21060cfb21b2b7662
Ryan Field <fieldryan19@gmail.com>
2025-07-09 17:41:17 +1200

    drizzle setup

---
+++ src/server/plugins/db-inirtialization.plugin.ts
@@ -0,0 +1,39 @@
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { defineNitroPlugin } from 'nitropack/runtime';
+import { usersTable } from '../../db/schema';
+import { eq } from 'drizzle-orm';
+
+export default defineNitroPlugin(async (nitroApp) => {
+  const db = drizzle(process.env.DATABASE_URL);
+
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!');
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users);
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log('User info updated!');
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log('User deleted!');
+});
